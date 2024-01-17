a9fde9028fd5d5c523d59139fbd0b1fed03228cd
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-01-17 15:40:44 +0100

    sal

--- app/Filters/CRM/Traits/NoteFilterTrait.php
+++ app/Filters/CRM/Traits/NoteFilterTrait.php
@@ -3,7 +3,7 @@
 namespace App\Filters\CRM\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
+
 
 trait NoteFilterTrait
 {
@@ -23,29 +23,6 @@ trait NoteFilterTrait
         });
     }
 
-    // public function hasLastNote($order = 'desc')
-    // {
-    //     // Define the order based on the input.
-    //     $order = $order == "oldest" ? 'asc' : 'desc';
-
-    //     // Create a subquery that gets the latest or oldest note for each user.
-    //     $notesSubQuery = DB::table('notes')
-    //         ->selectRaw('noteable_id, MAX(created_at) as latest_note_date')
-    //         ->groupBy('noteable_id');
-
-    //     // Join this subquery with the main query.
-    //     $data = $this->builder
-    //         ->joinSub($notesSubQuery, 'note_sub', function ($join) {
-    //             $join->on('people.id', '=', 'note_sub.noteable_id');
-    //         })
-    //         ->with(['notes' => function ($query) use ($order) {
-    //             $query->orderBy('created_at', $order)->take(1);
-    //         }])
-    //         ->select('people.*', 'note_sub.latest_note_date');
-
-    //     return $data;
-    // }
-
     public function hasLastNote($order = 'desc')
     {
         if ($order == "oldest") {
@@ -54,22 +31,16 @@ trait NoteFilterTrait
             $order = 'desc';
         }
 
-        // Subquery to get the latest or oldest note id for each user
-        $notesSubQuery = DB::table('notes')
-            ->selectRaw('MAX(id) as last_note_id, noteable_id')
-            ->groupBy('noteable_id');
-
         $data = $this->builder
-            ->joinSub($notesSubQuery, 'latest_notes', 'latest_notes.noteable_id', '=', 'people.id')
-            ->join('notes', 'notes.id', '=', 'latest_notes.last_note_id')
-            ->select('people.*', 'notes.created_at as last_note_date')
-            ->orderBy('last_note_date', $order);
+            ->whereHas('notes') // Ensure the user has notes
+            ->with(['notes' => function ($query) use ($order) {
+                $query->orderBy('created_at', $order) // Order the notes by creation date
+                   ;
+                  
+            }]);
 
         return $data;
     }
-
-
-
 
 
 
